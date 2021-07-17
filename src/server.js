e66d7f88f1904c15114e804586ca000ecddbf3a8
Hyuno Choi <soonitoon@gmail.com>
2021-07-17 22:55:57 +0900

    Feat: Make server get root request

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => {
+    console.log("Somebody to trying to go home");
+}
+
+app.get("/", handleHome);
+
 const handleListening = () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 }
