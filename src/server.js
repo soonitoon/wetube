ef3e0ffc45d0067dc9810346d596e00e8138b6de
Hyuno Choi <soonitoon@gmail.com>
2021-07-17 23:55:34 +0900

    Feat: Make more BIG middleware

--- src/server.js
+++ src/server.js
@@ -4,8 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`someone is going to ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not allowed</h1>");
+    }
     next();
 }
 
@@ -17,8 +25,10 @@ const handleLogin = (req, res) => {
     return res.send("<h1>login here</h1>");
 }
 
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", gossipMiddleware, handleLogin);
+app.get("/login", handleLogin);
 
 const handleListening = () => {
     console.log(`Server listening on http://localhost:${PORT}`);
