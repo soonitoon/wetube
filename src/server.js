ebb65dd61977312e0ff760e15453e2e994238ba7
Hyuno Choi <soonitoon@gmail.com>
2021-07-18 00:15:48 +0900

    Feat: Clear previous middlewares

--- src/server.js
+++ src/server.js
@@ -4,19 +4,6 @@ const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if (url === "/protected") {
-        return res.send("<h1>Not allowed</h1>");
-    }
-    next();
-}
-
 const handleHome = (req, res) => {
     return res.send("bye.");
 }
@@ -25,8 +12,6 @@ const handleLogin = (req, res) => {
     return res.send("<h1>login here</h1>");
 }
 
-app.use(logger);
-app.use(privateMiddleware);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
 
