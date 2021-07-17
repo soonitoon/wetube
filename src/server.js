245c686c8d27aafc7a902f028d3861e7b480c37f
Hyuno Choi <soonitoon@gmail.com>
2021-07-18 01:44:48 +0900

    Feat: Delete practice code

--- src/server.js
+++ src/server.js
@@ -6,17 +6,7 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-    return res.send("bye.");
-}
-
-const handleLogin = (req, res) => {
-    return res.send("<h1>login here</h1>");
-}
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
 
 const handleListening = () => {
     console.log(`Server listening on http://localhost:${PORT}`);
