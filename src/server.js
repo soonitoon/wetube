205023c9c5848028f021c904e539d02d9e209409
Hyuno Choi <soonitoon@gmail.com>
2021-07-17 23:34:24 +0900

    Feat: Add simple middleware

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`someone is going to ${req.url}`);
+    next();
+}
+
 const handleHome = (req, res) => {
     return res.send("bye.");
 }
@@ -13,7 +18,7 @@ const handleLogin = (req, res) => {
 }
 
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/login", gossipMiddleware, handleLogin);
 
 const handleListening = () => {
     console.log(`Server listening on http://localhost:${PORT}`);
