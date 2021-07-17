b99f821f2cde74dec0c9aae3751f8928e4604bcf
Hyuno Choi <soonitoon@gmail.com>
2021-07-18 01:55:24 +0900

    Feat: Add GET request handler to routers

--- src/server.js
+++ src/server.js
@@ -9,8 +9,22 @@ const logger = morgan("dev");
 app.use(logger);
 
 const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
 const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/users", handleEditUser);
+
 const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/videos", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
