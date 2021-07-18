7cf16104646cecfb67b6735ee9746054f00783f9
Hyuno Choi <soonitoon@gmail.com>
2021-07-18 22:31:41 +0900

    Feat: Make /edit in videoRouter

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,7 +3,9 @@ import express from "express";
 const videoRouter = express.Router();
 
 const handleWatchVideo = (req, res) => res.send("Watch Video");
+const handleEdit = (req, res) => res.send("Edit Video");
 
 videoRouter.get("/watch", handleWatchVideo);
+videoRouter.get("/edit", handleEdit);
 
 export default videoRouter;
