74e5a73b27992602bffbaf7388d60539fbeb28aa
Hyuno Choi <soonitoon@gmail.com>
2021-07-18 22:50:42 +0900

    Feat: Delete practice controller and make new controller

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
+import trending from "../controllers/videoController";
+import join from "../controllers/userController";
 
 const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send("Home");
-const handleJoin = (req, res) => res.send("Join");
-
-globalRouter.get("/", handleHome);
-globalRouter.get("/join", handleJoin);
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
 
 export default globalRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,9 +2,6 @@ import express from "express";
 
 const videoRouter = express.Router();
 
-const handleWatch = (req, res) => res.send("Watch Video");
-const handleEdit = (req, res) => res.send("Edit Video");
-
 videoRouter.get("/watch", handleWatch);
 videoRouter.get("/edit", handleEdit);
 
