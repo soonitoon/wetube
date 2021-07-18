8e556802fccb6fe03b5b9e677830fe0eaf0c8b14
Hyuno Choi <soonitoon@gmail.com>
2021-07-18 23:31:50 +0900

    Feat: Make login controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
 export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("Login");
 export const edit = (req, res) => res.send("Edit user");
 export const remove = (req, res) => res.send("Remove user");
