805c8b0f2608ae86c847fca972fc2047898844bc
Hyuno Choi <soonitoon@gmail.com>
2021-07-18 22:33:57 +0900

    Feat: Make /delete in userRouter

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,7 +3,9 @@ import express from "express";
 const userRouter = express.Router();
 
 const handleEditUser = (req, res) => res.send("Edit User");
+const handleDelete = (req, res) => res.send("Delete User");
 
 userRouter.get("/edit", handleEditUser);
+userRouter.get("/delete", handleDelete);
 
 export default userRouter;
