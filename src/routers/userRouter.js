12822f8d24c1712e9b035fec28aac8426ee4f25e
Hyuno Choi <soonitoon@gmail.com>
2021-07-18 23:36:43 +0900

    Feat: Make new URLs to userRouter

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,7 +3,9 @@ import { edit, remove } from "../controllers/videoController";
 
 const userRouter = express.Router();
 
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get("/:id", see);
 
 export default userRouter;
