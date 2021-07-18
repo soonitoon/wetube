db25286181e1aa50f9eadd5f25dad0e060844723
Hyuno Choi <soonitoon@gmail.com>
2021-07-18 23:46:36 +0900

    Feat: Make new controllers for videoRouter

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,3 +2,5 @@ export const trending = (req, res) => res.send("Home Page videos");
 export const see = (req, res) => res.send("Watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => res.send("deleteVideo");
