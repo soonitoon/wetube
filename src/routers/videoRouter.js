00b16d1919bcd9c2e0e9fd41fd73fb19a7e6553c
Hyuno Choi <soonitoon@gmail.com>
2021-07-18 23:42:22 +0900

    Style: Change name of controller watch to see

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { watch, edit } from "../controllers/videoController";
+import { see, edit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
+videoRouter.get("/:id", see);
 videoRouter.get("/edit", edit);
 
 export default videoRouter;
