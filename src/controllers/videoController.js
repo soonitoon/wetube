c1fece7765dc4f923db85d01f82540dea84e88dc
Hyuno Choi <soonitoon@gmail.com>
2021-07-19 23:57:51 +0900

    Feat: Make controller render pug files

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,9 @@
 export const trending = (req, res) => {
   res.render("home");
 };
-export const see = (req, res) => res.send("Watch");
+export const see = (req, res) => {
+  res.render("watch");
+};
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
