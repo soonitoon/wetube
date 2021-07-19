aee10dbeede8a69303f2faa53dc254d67fdc6fb8
Hyuno Choi <soonitoon@gmail.com>
2021-07-19 23:24:39 +0900

    Feat: Make trending controller render home

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,6 @@
-export const trending = (req, res) => res.send("Home Page videos");
+export const trending = (req, res) => {
+  res.render("home");
+};
 export const see = (req, res) => res.send("Watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
