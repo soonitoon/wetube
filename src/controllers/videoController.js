46d41385b308961223f7042e7e8921a624acc5fa
Hyuno Choi <soonitoon@gmail.com>
2021-07-20 00:17:14 +0900

    Feat: Make edit video controller render pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,9 @@ export const trending = (req, res) => {
 export const see = (req, res) => {
   res.render("watch");
 };
-export const edit = (req, res) => res.send("Edit");
+export const edit = (req, res) => {
+  res.render("edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => res.send("deleteVideo");
