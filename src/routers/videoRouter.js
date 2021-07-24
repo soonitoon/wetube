4d6427a0bcc1d3ffdf010a1ff1937beead652bc8
Hyuno Choi <soonitoon@gmail.com>
2021-07-25 02:25:49 +0900

    Feat: Make video upload function just practice!

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { see, getEdit, postEdit } from "../controllers/videoController";
+import {
+  see,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", see);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
