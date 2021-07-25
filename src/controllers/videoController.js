46d0704c6455fd46213f6a93dd5f8a3cac3ccb20
Hyuno Choi <soonitoon@gmail.com>
2021-07-25 23:08:16 +0900

    Chore: Delete code that use videos array

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,29 +1,26 @@
 export const trending = (req, res) => {
   return res.render("home", {
     pageTitle: "Home",
-    videos,
   });
 };
+
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
   return res.render("watch", {
-    pageTitle: `Watching: ${video.title}`,
-    video,
+    pageTitle: `Watching`,
   });
 };
+
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
   return res.render("edit", {
-    pageTitle: `Editing: ${video.title}`,
-    video,
+    pageTitle: `Editing`,
   });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -35,14 +32,5 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "Just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
