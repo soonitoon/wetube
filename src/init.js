b9dc0a776417d402158a05d619e2c61b5d286238
Hyuno Choi <soonitoon@gmail.com>
2021-07-25 23:05:33 +0900

    Feat: Separate initialize function to init.js

---
+++ src/init.js
@@ -0,0 +1,11 @@
+import "./db";
+import "./models/video";
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
