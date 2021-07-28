8d59b66c5fa4fbaedcebd63e10b6ffc75c4f5281
Hyuno Choi <soonitoon@gmail.com>
2021-07-28 23:50:15 +0900

    Feat: Add trim method and clean code

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,13 +1,38 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  createdAt: { type: Date, required: true, default: Date.now },
-  hashtags: [{ type: String }],
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  hashtags: [
+    {
+      type: String,
+      trim: true,
+    },
+  ],
   meta: {
-    views: { type: Number, default: 0, required: true },
-    rating: { type: Number, default: 0, required: true },
+    views: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
   },
 });
 
