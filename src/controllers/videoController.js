43341b301d5fa124ce3cd624d08a1c57e085cb51
Hyuno Choi <soonitoon@gmail.com>
2021-08-02 01:19:55 +0900

    Feat: Make search video function

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -79,9 +79,16 @@ export const getDelete = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: { $regex: new RegExp(keyword, "i") },
+    });
+  }
   return res.render("search", {
     pageTitle: "Search",
+    videos,
   });
 };
