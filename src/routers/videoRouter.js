da89292c65d2f719f9867ee500f7f7c931eda273
Hyuno Choi <soonitoon@gmail.com>
2021-08-04 00:12:40 +0900

    Chore: Delete unnecessary route

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -12,7 +12,7 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(getDelete);
+videoRouter.get("/:id([0-9a-f]{24})/delete", getDelete);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
