dc840b4a6075432f1b2155bb9bd28178a5e3f21e
Hyuno Choi <soonitoon@gmail.com>
2021-08-08 22:53:42 +0900

    Feat: Make base of join page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
-export const join = (req, res) => res.send("Join");
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = (req, res) => {};
 export const login = (req, res) => res.send("Login");
 export const edit = (req, res) => res.send("Edit user");
 export const remove = (req, res) => res.send("Remove user");
