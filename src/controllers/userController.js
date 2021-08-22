d46e6643b8c5ce7e4e34965b25587f24001ca515
Hyuno Choi <soonitoon@gmail.com>
2021-08-22 15:26:24 +0900

    Feat: Make username/email validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,14 @@ import User from "../models/User";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   const { name, username, email, password, location } = req.body;
+  const pageTitle = "Join";
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
   await User.create({
     name,
     username,
